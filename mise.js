77e14c507a87faadd432ca3776886a9c3e486995
sowasp <sowsmartool@gmail.com>
2026-08-10 16:29:57 +0200

    finished the basic setting for the newDish button

--- mise.js
+++ mise.js
@@ -5,10 +5,9 @@ const dishes = document.querySelector("#dishes")
 button.addEventListener("click", function(){
     dishCount ++;
     const dishId = `dish-${dishCount}`;
-
     const newDish = document.createElement("fieldset");
+    
     const legend = document.createElement("legend");
-
     legend.textContent = "dish";
     newDish.appendChild(legend);
 
@@ -16,17 +15,39 @@ button.addEventListener("click", function(){
     dishLabel.textContent = "Dish name";
     dishLabel.setAttribute("for", dishId);
     newDish.appendChild(dishLabel);
-
     const dishInput = document.createElement("input");
     dishInput.setAttribute("type", "text");
     dishInput.setAttribute("id", dishId);
     newDish.appendChild(dishInput);
 
+    const popLabel = document.createElement("label");
+    popLabel.textContent = "Popularity";
+    popLabel.setAttribute("for", "popularity");
+    newDish.appendChild(popLabel);
+    const popInput = document.createElement("input");
+    popInput.setAttribute("type", "number");
+    popInput.setAttribute("id", "popularity");
+    popInput.setAttribute("min", "0");
+    popInput.setAttribute("max", "100");
+    newDish.appendChild(popInput);
+
+    const stockLabel = document.createElement("label");
+    stockLabel.textContent = "Current stock";
+    stockLabel.setAttribute("for", "stock");
+    newDish.appendChild(stockLabel);
+    const stockInput = document.createElement("input");
+    stockInput.setAttribute("type", "number");
+    stockInput.setAttribute("id", "stock");
+    stockInput.setAttribute("min", "0");
+    stockInput.setAttribute("step", "1");
+    newDish.appendChild(stockInput);
+
+
     dishes.appendChild(newDish);
 
     
 
-    
+  
     
 
 })
